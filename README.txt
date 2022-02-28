456743e778e8661d817a0f46e03e3d473d4d95fb
Jericho Locke <jljericho@gmail.com>
2022-02-28 07:21:58 -0500

    Started an outline of my class

---
+++ README.txt
@@ -0,0 +1,2 @@
+I want to be able to systematically specify how I am extracting objects from a dictionary, specifically in
+a way that is easy to change.
---
+++ extractor.py
@@ -0,0 +1,12 @@
+
+
+class Extractor:
+
+    def __init__(self, schema: dict):
+        self.schema = schema
+
+    def _validate_schema(self):
+        pass
+
+    def extract(self, data: dict):
+        pass
